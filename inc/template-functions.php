d0f6b27401bef04562d51fdb1bc11cd4910c2145
alaczek <ola@bodera.com>
2017-06-15 09:53:34 +1000

    Add "Continue reading" link to post excerpts as per TUX list.

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -25,3 +25,19 @@ function _s_body_classes( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', '_s_body_classes' );
+
+/**
+ * Replaces "[...]" (appended to automatically generated excerpts) with ... and a 'Continue reading' link.
+ * @return string 'Continue reading' link prepended with an ellipsis.
+ */
+if ( ! function_exists( '_s_excerpt_more' ) ) :
+	function _s_excerpt_more( $more ) {
+		$link = sprintf( '<a href="%1$s" class="more-link">%2$s</a>',
+			esc_url( get_permalink( get_the_ID() ) ),
+			/* translators: %s: Name of current post */
+			sprintf( esc_html__( 'Continue reading %s', '_s' ), '<span class="screen-reader-text">' . get_the_title( get_the_ID() ) . '</span>' )
+			);
+		return ' &hellip; ' . $link;
+	}
+	add_filter( 'excerpt_more', '_s_excerpt_more' );
+endif;
