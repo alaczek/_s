726b0790c7d45896a1e8a2dffe802a0267cf867c
alaczek <ola@bodera.com>
2017-06-15 09:47:14 +1000

    Include all the Content Options settings - seems easier to remove the options you don't need while building a theme, then looking up docs every time.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -26,21 +26,26 @@ function _s_jetpack_setup() {
 
 	// Add theme support for Content Options.
 	add_theme_support( 'jetpack-content-options', array(
-		//'blog-display'	  => 'content',
-		'author-bio'      => true,
-		'post-details'    => array(
-			'stylesheet' => '_s-style',
-			'date'       => '.posted-on',
-			'categories' => '.cat-links',
-			'tags'       => '.tags-links',
-			'author'     => '.byline',
-			//'comment'	 => '.comments-link',
+		'blog-display'       => 'content', // the default setting of the theme: 'content', 'excerpt' or array( 'content', 'excerpt' ) for themes mixing both display.
+		'author-bio'         => true, // display or not the author bio: true or false.
+		'author-bio-default' => false, // the default setting of the author bio, if it's being displayed or not: true or false (only required if false).
+		'masonry'            => '.site-main', // a CSS selector matching the elements that triggers a masonry refresh if the theme is using a masonry layout.
+		'post-details'       => array(
+			'stylesheet'      => 'themeslug-style', // name of the theme's stylesheet.
+			'date'            => '.posted-on', // a CSS selector matching the elements that display the post date.
+			'categories'      => '.cat-links', // a CSS selector matching the elements that display the post categories.
+			'tags'            => '.tags-links', // a CSS selector matching the elements that display the post tags.
+			'author'          => '.byline', // a CSS selector matching the elements that display the post author.
+			'comment'         => '.comments-link', // a CSS selector matching the elements that display the comment link.
 		),
-		'featured-images' => array(
-			'archive'      => true,
-			'post'         => true,
-			'page'         => true,
-			'fallback'     => true,
+		'featured-images'    => array(
+			'archive'         => true, // enable or not the featured image check for archive pages: true or false.
+			'archive-default' => false, // the default setting of the featured image on archive pages, if it's being displayed or not: true or false (only required if false).
+			'post'            => true, // enable or not the featured image check for single posts: true or false.
+			'post-default'    => false, // the default setting of the featured image on single posts, if it's being displayed or not: true or false (only required if false).
+			'page'            => true, // enable or not the featured image check for single pages: true or false.
+			'page-default'    => false, // the default setting of the featured image on single pages, if it's being displayed or not: true or false (only required if false).
+			'fallback'		  => true, // enable fallack to first image used in the post.
 		),
 	) );
 }
